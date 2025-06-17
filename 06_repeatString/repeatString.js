5b7a124ba78d6554b188ce73f4d3a17a564797f8
somber-robot <somberrobot@gmail.com>
2025-06-17 23:12:27 +0100

    Complete exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) return "ERROR";
+    let result = "";
+    for (i = 0; i < count; i++) result += str;
+    return result;
 };
 
 // Do not edit below this line
