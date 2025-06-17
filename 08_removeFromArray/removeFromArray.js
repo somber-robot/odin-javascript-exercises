08bd6e66258a843d4dfb6a34b61856b5c0e244c7
somber-robot <somberrobot@gmail.com>
2025-06-17 23:35:09 +0100

    Complete exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    for (i = 1; i < arguments.length; i++){
+        while (arguments[0].includes(arguments[i])){
+            let index = arguments[0].indexOf(arguments[i]);
+            if (index !== -1) arguments[0].splice(index, 1);
+        }
+    }
+    return arguments[0];
 };
 
 // Do not edit below this line
