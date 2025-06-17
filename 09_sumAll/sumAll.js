f79ec9ca39b03046d06e97af46faea6e207fa415
somber-robot <somberrobot@gmail.com>
2025-06-17 23:46:04 +0100

    Complete exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 
+              || a != Math.floor(a) 
+              || b != Math.floor(b)
+              || typeof(a) != "number"
+              || typeof(b) != "number"
+    ) return "ERROR";
 
+    let sum = 0;
+    let start, end;
+    if (a < b) { start = a; end = b;}
+    else {start = b, end = a}
+    for (i = start; i <= end; i++) sum += i;
+    return sum;
 };
 
 // Do not edit below this line
