83627ed73a978bcd9ed347eaef11a455c1c26a3b
somber-robot <somberrobot@gmail.com>
2025-06-20 17:32:41 +0100

    Complete exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    newStr = Array.from(str).filter(letter => isAlpha(letter));
+    str = newStr.toString().toLowerCase();
+    for (i = 0; i < str.length/2; i++){
+        if (str[i] !== str[str.length-1-i]) return false;
+    }
+    return true;
 };
+
+function isAlpha(str){
+    return /^[A-Za-z0-9]+$/.test(str);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
