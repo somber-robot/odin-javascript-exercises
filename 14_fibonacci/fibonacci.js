e817d8ae656ca6ecdbbb36cb375ea10ad2480484
somber-robot <somberrobot@gmail.com>
2025-06-20 17:46:11 +0100

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(pos) {
+    pos = +pos;
+    if (pos < 0) return "OOPS";
+    if (pos == 0) return 0;
 
+    let counter = 2;
+    let x = 1, y = 1;
+    while(counter < pos){
+        temp = x;
+        x = y;
+        y += temp;
+        counter++;
+    }
+    return y;
 };
 
 // Do not edit below this line
