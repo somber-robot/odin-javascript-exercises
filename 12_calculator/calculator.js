584af690ec794ff112d844a9305bde8d2b6dfa5a
somber-robot <somberrobot@gmail.com>
2025-06-20 17:15:49 +0100

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    return arr.reduce((prod, current) => prod * current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+    return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    if (num === 0) return 1;
+    let fact = 1;
+    for (i = 2; i <= num; i++) fact *= i;
+    return fact;
 };
 
 // Do not edit below this line
